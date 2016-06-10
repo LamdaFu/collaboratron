22c57ec6c13a2e419c1b011bfab4e38c40b71f1b
Matt Pouttu-Clarke <voidpointer101@gmail.com>
2016-06-10 14:13:13 -0700

    Update docs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lamdafu/collaboratron "0.1.0-SNAPSHOT" 
   :description "λ夫 - Collaboratron: Fuzzy set theory based self-organizing λ engine"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/LamdaFu/collaboratron"
   :license {:name "Apache 2.0"
             :url "https://github.com/LamdaFu/collaboratron/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
